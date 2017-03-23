7878523f4e765eeca117e412bede8f5b93f242fa
Angelo Bovino <angelo.bovino@outlook.com>
2017-03-22 22:49:16 -0400

    axios get request to retrieve local api data

---
+++ serverRender.js
@@ -0,0 +1,10 @@
+// fetch data from the api
+
+import config from './config';
+import axios from 'axios';
+
+axios.get(`${config.serverUrl}/api/contests`)
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(console.error)
